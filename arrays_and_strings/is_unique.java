9f353e9b6be86531306369a952da564d6d850522
diogofernandesc <diogofernandescon@gmail.com>
2021-03-10 19:13:35 +0000

    poor solution

--- arrays_and_strings/is_unique.java
+++ arrays_and_strings/is_unique.java
@@ -22,3 +22,25 @@ class Solution {
 }
 
 
+// Without the hashset it's O(n^2)
+class Solution2 {
+    public boolean isUnique(String str) {
+        // Assumes standard ASCII character set
+        if (str.length() > 128) {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char val = str.charAt(i);
+            for (int j = i+1; j < str.length(); j++) {
+                char next = str.charAt(j);
+                if (val == next) {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+}
+
+
