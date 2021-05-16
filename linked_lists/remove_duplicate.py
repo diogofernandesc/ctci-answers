5a870a9d0c90997e5ab65af2bb171e330c2348e2
diogofernandesc <diogofernandescon@gmail.com>
2021-05-16 17:18:35 +0100

    linked lists: remove duplicates no buffer py

--- linked_lists/remove_duplicate.py
+++ linked_lists/remove_duplicate.py
@@ -9,3 +9,16 @@ def remove_duplicates(node):
             previous_node = node
 
         node = node.next
+
+
+# No buffer solution
+def remove_duplicates(head):
+    current_node = head
+    while (current_node is not None):
+        runner_node = current_node.next
+        while(runner_node is not None):
+            if (current_node.data == runner_node.data):
+                runner_node.next = runner_node.next.next
+            else:
+                runner_node = runner_node.next
+        current_node = current_node.next
